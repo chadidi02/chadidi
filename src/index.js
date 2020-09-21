a5a5a3ecb1745ade982209bfb331e71e73c3e54f
Souhaib Chadidi <souhaibchadidi02@gmail.com>
2020-09-21 21:44:35 +0100

    🤦‍♂️

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import "./css/tailwind.css";
+import "./app.js";
+// pages
 import "./views/components/home";
 import "./views/components/about";
-import "./app.js";
+import "./views/components/portfolio";
+import "./views/components/blog";
+import "./views/components/contact";
